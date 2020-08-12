4834d963fcbd0cce55566273f26a0e80066c0217
riyandharne-123 <60026968+riyandharne-123@users.noreply.github.com>
2020-08-12 11:58:21 +0530

    Add files via upload

---
+++ precache-manifest.9fa91d496c22afcb72901b4085f0cfb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e908f5f4e0543b377da95f0dbf99662",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15ec61733636a24b4da8",
+    "url": "/static/css/main.b056db4f.chunk.css"
+  },
+  {
+    "revision": "abbbb31b6182b86e8aeb",
+    "url": "/static/js/2.086bffc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.086bffc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ec61733636a24b4da8",
+    "url": "/static/js/main.89a7fbb0.chunk.js"
+  },
+  {
+    "revision": "252f969779de0734efb3",
+    "url": "/static/js/runtime-main.75211130.js"
+  }
+]);
